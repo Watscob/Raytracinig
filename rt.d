e5065e2ae86a5ac3f351934a9e38112303493b9d
Etienne Chanudet <etienne.chanudet@epita.fr>
2020-12-22 10:52:13 +0100

    initial_commit

---
+++ rt.d
@@ -0,0 +1,7 @@
+rt.o: rt.c includes/bmp.h includes/image.h includes/utils/alloc.h \
+ includes/camera.h includes/ray.h includes/vec3.h \
+ includes/normal_material.h includes/object.h includes/utils/refcnt.h \
+ includes/obj_loader.h includes/scene.h includes/utils/pvect.h \
+ includes/utils/gvect.h includes/utils/gvect_common.h \
+ includes/utils/pvect_wrap.h includes/phong_material.h includes/sphere.h \
+ includes/triangle.h
---
+++ src/phong.d
@@ -0,0 +1,4 @@
+src/phong.o: src/phong.c includes/phong_material.h includes/object.h \
+ includes/ray.h includes/vec3.h includes/utils/refcnt.h includes/scene.h \
+ includes/camera.h includes/utils/pvect.h includes/utils/gvect.h \
+ includes/utils/gvect_common.h includes/utils/pvect_wrap.h
