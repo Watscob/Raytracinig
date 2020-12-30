f8c5b2cbb2abebdcee86aa1eb073c4c9eac74c5e
Etienne Chanudet <etienne.chanudet@epita.fr>
2020-12-30 19:28:45 +0100

    clang-format all files

--- includes/procedural_background.h
+++ includes/procedural_background.h
@@ -7,6 +7,8 @@
 void init_seed(int x);
 void generate_noise_map(size_t width, size_t height, float scale);
 void free_noise_map(void);
-struct rgb_pixel get_procedural_pixel(struct scene *scene, struct rgb_image *image, size_t x, size_t y);
+struct rgb_pixel get_procedural_pixel(struct scene *scene,
+                                      struct rgb_image *image, size_t x,
+                                      size_t y);
 
 #endif /* PROCEDURAL_BACKGROUND_H */
